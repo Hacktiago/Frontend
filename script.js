f4d43fd1022d68076d49bc02c6c4b5a045fb68ed
Hacktiago <david.rivera04@uptc.edu.co>
2024-02-27 17:29:08 -0500

    fetch a get y patch added

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// script.js
-const form = document.getElementById("form");
+
 const enviarBtn = document.getElementById("enviar-btn")
+const removeBtn = document.getElementById("delete-car-btn");
+const getCarsBtn = document.getElementById("get-cars-btn");
 
 enviarBtn.addEventListener('click', submitForm);
+removeBtn.addEventListener('click', removeCar);
+getCarsBtn.addEventListener('click', getCars);
 
 function submitForm(e) {
-    console.log("Entre a la funcion del front")
     e.preventDefault();
     const licensePlate = document.getElementById("licensePlate");
     const color = document.getElementById("color");
@@ -23,76 +25,49 @@ function submitForm(e) {
         .catch((err) => ("Error occured", err));
 }
 
-// document.getElementById("getFilesBtn").addEventListener("click", getFileList);
+// script.js
+async function getCars() {
+    const response = await fetch('http://localhost:3000/cars');
+    const carDescriptions = await response.json();
+    const carsList = document.getElementById('carros');
+    carDescriptions.forEach(car => {
+        const item = document.createElement('li');
+        const pColor = document.createElement('p');
+        pColor.innerHTML = 'color: ' + car.color;
+        
+        const pPlaca = document.createElement('p');
+        pPlaca.innerHTML = 'placa: ' + car.license_plate;
 
-// function getFileLihst() {
-//     console.log("Soy el del front");
-//     fetch("http://localhost:5000/getFiles")
-//         .then(response => {
-//             console.log(response.body)
-//         })
-//         .catch(error => console.error("Error al obtener la lista de archivos:", error));
-// }
+        
+        const pPath = document.createElement('p');
+        pPath.innerHTML = 'ruta imagen: ' + car.photoPath;
 
+        const pTime = document.createElement('p');
+        pTime.innerHTML = 'Hora: ' + car.currentTime;
+        item.appendChild(pPlaca);
+        item.appendChild(pPath);
+        item.appendChild(pColor);
+        item.appendChild(pTime);
 
-// // Función para obtener la lista de archivos del servidor
-// function getFileList() {
-//     console.log("Soy el del front");
-//     fetch("http://localhost:5000/getFiles")
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data.files[0]); // Imprimir los datos en la consola para verificar
-//             displayFileList(data.files[0]); // Llamar a una función para mostrar la lista de archivos en la interfaz de usuario
-//         })
-//         .catch(error => console.error("Error al obtener la lista de archivos:", error));
-// }
+        carsList.appendChild(item);
+    });
+}
 
-
-// // Función para mostrar la lista de archivos en la interfaz de usuario
-// function displayFileList(files) {
-//     const path = 'uploads/'
-//     console.log(files)
-//     console.log(typeof(files))
-//     const fileListElement = document.getElementById("fileList");
-//     fileListElement.innerHTML = ""; // Limpiar la lista de archivos antes de mostrar nuevos datos
-
-//     var imagen = document.createElement("img")
-
-//     imagen.src = path+files; // Reemplaza con la ruta de tu imagen
-//     fileListElement.appendChild(imagen);
-//     //acceder al atributo 
-
-
-//     // Iterar sobre la lista de archivos y agregar cada archivo como un elemento de lista
-//     // files.forEach(file => {
-//     //     const listItem = document.createElement("li");
-//     //     listItem.textContent = file;
-//     //     fileListElement.appendChild(listItem);
-//     // });
-
-// }
-
-
-// scripts.js
-
-// async function submitForm() {
-//     const form = document.getElementById('carForm');
-//     const formData = new FormData(form);
+function removeCar(e) {
+    e.preventDefault();
+    const licensePlate = document.getElementById("toRemovelicensePlate");
+    const request =   {
+        "license_plate": licensePlate.value
+        }
+    const requestJSON = JSON.stringify(request);
+    fetch("http://localhost:3000/cars", {
+        method: 'PATCH',
+        body: requestJSON,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then((res) => console.log(res))
+        .catch((err) => console.error("Error occurred", err));
+}
   
-//     try {
-//       const response = await fetch('http://localhost:3000/cars', {
-//         method: 'POST',
-//         body: formData,
-//       });
-  
-//       const result = await response.json();
-//       console.log(result);
-  
-//       // Aquí puedes manejar la respuesta según tus necesidades
-//       alert(result.message);
-//     } catch (error) {
-//       console.error('Error al enviar el formulario:', error);
-//       // Puedes mostrar un mensaje de error al usuario si lo prefieres
-//       alert('Hubo un error al enviar el formulario');
-//     }
-//   }
